894678c71a524f0a962fa87e9e3f8c2763725ff3
AsifLangChaIN <archana.stsintern@gmail.com>
2024-07-26 17:17:01 +0530

    added code in file

---
+++ hmacshenlldot.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Company.Function
+{
+    public class hmacshenlldot
+    {
+        private readonly ILogger<hmacshenlldot> _logger;
+
+        public hmacshenlldot(ILogger<hmacshenlldot> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("hmacshenlldot")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+    }
+}
